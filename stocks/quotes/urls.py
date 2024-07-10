f7a83651461433801c0613d135040fbe484e2174
William Uvlin <Will.Uvlin@gmail.com>
2024-07-09 22:38:16 -0400

    Create about me page and paths to it

--- stocks/quotes/urls.py
+++ stocks/quotes/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     #path to home page
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    #path to about page
+    path('about.html', views.about, name="about"),
 
 ]
--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 #broswer request for home page
 def home(request):
     return render(request, 'home.html', {})
+
+#browser request for about page
+def about(request):
+    return render(request, 'about.html', {})
