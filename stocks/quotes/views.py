93c1fe6fb717c61e9a543f84c2d10cae0627a086
William Uvlin <Will.Uvlin@gmail.com>
2024-07-10 22:44:24 -0400

    Grab API keys for potential APIs

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+#Intrinio, 2 week free then must pay...
+#d537a92d9a301f16e1bf3070daf481bb
+#Alpha Vantage API (Potentially use)
+#SYITEWAX5PIPY5ZO
+#Polygon.io (prob use this one)
+#UjyFDQigBbrjV_NfEPRc9kDC3SlqEsxR
 #broswer request for home page
 def home(request):
     return render(request, 'home.html', {})
