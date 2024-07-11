8875e0d4977e9a2c50fb489919b17ed5b436fd2e
William Uvlin <Will.Uvlin@gmail.com>
2024-07-11 19:56:26 -0400

    Hide api key and have api_request be taken out of the list foe easier calling

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,31 +1,20 @@
 from django.shortcuts import render
-
-#Intrinio, 2 week free then must pay...
-#d537a92d9a301f16e1bf3070daf481bb
-
-#Alpha Vantage API (Potentially use)
-#SYITEWAX5PIPY5ZO
-
-#Polygon.io (prob use this one)
-#UjyFDQigBbrjV_NfEPRc9kDC3SlqEsxR
-
-#finazon
-#7abf30fc13de4a8c98e2d3c201b43289yp
-
-#tiingo
-#53125623df24601520fe3ccf70180b07efd62258
+from django.conf import settings
 
 #broswer request for home page
 def home(request):
     import requests
     import json
 
-    api_requests = requests.get("https://api.tiingo.com/iex/?tickers=aapl,&token=53125623df24601520fe3ccf70180b07efd62258")
-    apiKey = '53125623df24601520fe3ccf70180b07efd62258'
+    #api_key taken from .env file
+    api_key = settings.TIINGO_API_KEY
+    url = f"https://api.tiingo.com/iex/?tickers=aapl&token={api_key}"
+    api_request = requests.get(url)
 
-    try:
-        api_requests.raise_for_status()
-        api = api_requests.json()
+    try:  
+        # turn the api request into a dictionary and pull it out of the list its in
+        api = api_request.json()[0]
+
     except Exception as e:
         api = "Error..."
     return render(request, 'home.html', {'api': api})
--- stocks/stocks/settings.py
+++ stocks/stocks/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TIINGO_API_KEY = config('TIINGO_API_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
