96f481b991466b9fd8d11eb56d9dcea52a4f30af
William Uvlin <Will.Uvlin@gmail.com>
2024-07-10 23:00:07 -0400

    create call to the polygon API for AAPl stock

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 
 #Intrinio, 2 week free then must pay...
 #d537a92d9a301f16e1bf3070daf481bb
+
 #Alpha Vantage API (Potentially use)
 #SYITEWAX5PIPY5ZO
+
 #Polygon.io (prob use this one)
 #UjyFDQigBbrjV_NfEPRc9kDC3SlqEsxR
+
 #broswer request for home page
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    api_requests = requests.get("https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?apiKey=UjyFDQigBbrjV_NfEPRc9kDC3SlqEsxR")
+    apiKey = 'UjyFDQigBbrjV_NfEPRc9kDC3SlqEsxR'
+
+    try:
+        api = json.loads(api_requests.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 #browser request for about page
 def about(request):
