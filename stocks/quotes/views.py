f8c4f2433698a978fe3fff67b1e3634b8b2dc3e6
William Uvlin <Will.Uvlin@gmail.com>
2024-07-16 22:02:31 -0400

    Allow searching for a specific ticker

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -6,18 +6,25 @@ def home(request):
     import requests
     import json
 
-    #api_key taken from .env file
-    api_key = settings.TIINGO_API_KEY
-    url = f"https://api.tiingo.com/iex/?tickers=aapl&token={api_key}"
-    api_request = requests.get(url)
+    #if someone filled out the form
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        #api_key taken from .env file
+        api_key = settings.TIINGO_API_KEY
+        #url to the ticker information we want
+        url = f"https://api.tiingo.com/iex/?tickers={ticker}&token={api_key}"
+        #request the info
+        api_request = requests.get(url)
 
-    try:  
-        # turn the api request into a dictionary and pull it out of the list its in
-        api = api_request.json()[0]
-
-    except Exception as e:
-        api = "Error..."
-    return render(request, 'home.html', {'api': api})
+        try:  
+            # turn the api request into a dictionary and pull it out of the list its in
+            api = api_request.json()[0]
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
+    #else as to enter a ticker
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
 
 #browser request for about page
 def about(request):
