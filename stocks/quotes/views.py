16eb3327d868229328dd790bfdfc4e195d00441d
William Uvlin <Will.Uvlin@gmail.com>
2024-07-17 23:00:11 -0400

    Connect the table to the API

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -35,22 +35,38 @@ def about(request):
 
 #browser request for add_stock page
 def add_stock(request):
-        # if someone filled the form and clicked the button
-        if request.method == 'POST':
-            # create a form variable with the ticker
-            form = StockForm(request.POST or None)
+    import requests
+    import json
+    # if someone filled the form and clicked the button
+    if request.method == 'POST':
+        # create a form variable with the ticker
+        form = StockForm(request.POST or None)
 
-            # check the form is valid
-            if form.is_valid():
-                # save to database
-                form.save()
-                # print success messgae
-                messages.success(request, ("Stock Has Been Added!"))
-                #return to add_stock page
-                return redirect('add_stock')
-        else:
-            ticker = Stock.objects.all()
-            return render(request, 'add_stock.html', {'ticker': ticker})
+        # check the form is valid
+        if form.is_valid():
+            # save to database
+            form.save()
+            # print success messgae
+            messages.success(request, ("Stock Has Been Added!"))
+            #return to add_stock page
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+        # lists of all the tickers that are decoded from the json
+        output = []
+        for ticker_item in ticker:
+            api_key = settings.TIINGO_API_KEY
+            url = f"https://api.tiingo.com/iex/?tickers={ticker_item}&token={api_key}"
+            #request the info
+            api_request = requests.get(url)
+            try:  
+                # turn the api request into a dictionary and pull it out of the list its in
+                api = api_request.json()[0]
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+            
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
 
 #function to delete stock from database
 def delete(request, stock_id):
